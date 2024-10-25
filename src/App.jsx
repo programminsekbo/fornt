136f8a86c4a82f16057074f530f449b0cf4cac09
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-10-25 00:59:47 -0700

    frontan

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 
 
 
-
 const App = () => {
-
-  let marck=prompt("giv me tou point")
   return (
     <div>
 
- <h1>{ marck>70? 
-  <h1> you are rigth  </h1> 
-  :<h1> you are rong  </h1> 
-  
-  }</h1>
+<h1>hello</h1>
+      
+        
+
+
+
+
 
     </div>
   );
