207334e9ab1f3528dccf92e5071b6fd664fd840e
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-10-28 06:54:29 -0700

    fromvalue

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,72 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
+
 
 
 const App = () => {
-
-
-  const [ NUMBER,NEWNUMBER]=useState({
-    key1:"value01",
-    key2:"value2",
-    key3:"value3"
-  })
+  let [number,setnumber]=useState({fName:"",lname:"",cite:"",gender:""})
 
 
 
-  const onchang=()=>{
 
-NEWNUMBER(
+const hridoy=(property,value)=>{
+  setnumber(prevOBJ=>({
 
-  prevesOBJ=>({
-
-...prevesOBJ,
-key1:"hello word"
-
-  })
-)
+    ...prevOBJ,
+    [property]:value
+  }))
 
 
-  }
 
+}
+
+const myvalue=(e)=>{
+  e.preventDefault();
+console.log(number)
+
+
+}
 
   return (
     <div>
 
-<h1>{NUMBER.key1}</h1>
 
-<button onClick={onchang}>clickhere</button>
-      
+<form onSubmit={myvalue}>
+
+<input onChange={(e)=>{hridoy("fName",e.target.value)}}  value={number.fName}  type="text" placeholder="fastname" />
+<input onChange={(e)=>{hridoy("lname",e.target.value)}}   value={number.lname} type="text" placeholder="lastname" /><br />
+
+<select value={number.cite} onChange={(e)=>{hridoy("cite",e.target.value)}} >
+<option value="">ahose dite</option>
+<option value="dhaka">dhaka</option>
+<option value="dinajpur">dinajpur</option>
+<option value="thakurgonj">thakurgonj</option>
+<option value="setabgonj">setabgonj</option>
+<option value="soidpur">soidpur</option>
+
+</select>
+
+<br />
+
+<input onChange={()=>{hridoy("gender","Mail")}}  checked={number.gender==="Mail"} type="radio" name="gender"/>Mail
+<input onChange={()=>{hridoy("gender",'femail')}}  checked={number.gender==="femail"} type="radio" name="gender"/>femail
+<br />
+
+<button>submit</button>
+
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+
     </div>
   );
 };
