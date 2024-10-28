f6348e5edcbce0037d2e44b6c2ef08253786d678
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-10-28 05:54:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,37 @@ import { useRef, useState } from "react";
 
 const App = () => {
 
-const [number,setnumber]=useState(0);
 
+  const [ NUMBER,NEWNUMBER]=useState({
+    key1:"value01",
+    key2:"value2",
+    key3:"value3"
+  })
+
+
+
+  const onchang=()=>{
+
+NEWNUMBER(
+
+  prevesOBJ=>({
+
+...prevesOBJ,
+key1:"hello word"
+
+  })
+)
+
+
+  }
 
 
   return (
     <div>
-  <h1>Number:{number}</h1>
-<br />
-<button onClick={()=>setnumber(number+1)}>click++</button>
-<button onClick={()=>setnumber(number-1)}>click--</button>
 
+<h1>{NUMBER.key1}</h1>
 
+<button onClick={onchang}>clickhere</button>
       
     </div>
   );
