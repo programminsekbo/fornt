df814716c82c6341a1125236f746596b4f2013b7
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-10-28 23:30:20 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,28 @@
-import { useState } from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Demo from './component/Demo';
+import Loging from './component/Loging';
+import Headar from './component/Headar';
 
 
 const App = () => {
-  let [number,setnumber]=useState({fName:"",lname:"",cite:"",gender:""})
-
-
-
-
-const hridoy=(property,value)=>{
-  setnumber(prevOBJ=>({
-
-    ...prevOBJ,
-    [property]:value
-  }))
-
-
-
-}
-
-const myvalue=(e)=>{
-  e.preventDefault();
-console.log(number)
-
-
-}
-
   return (
     <div>
+      
+<BrowserRouter>
+  <Routes>
 
-
-<form onSubmit={myvalue}>
-
-<input onChange={(e)=>{hridoy("fName",e.target.value)}}  value={number.fName}  type="text" placeholder="fastname" />
-<input onChange={(e)=>{hridoy("lname",e.target.value)}}   value={number.lname} type="text" placeholder="lastname" /><br />
-
-<select value={number.cite} onChange={(e)=>{hridoy("cite",e.target.value)}} >
-<option value="">ahose dite</option>
-<option value="dhaka">dhaka</option>
-<option value="dinajpur">dinajpur</option>
-<option value="thakurgonj">thakurgonj</option>
-<option value="setabgonj">setabgonj</option>
-<option value="soidpur">soidpur</option>
-
-</select>
-
-<br />
-
-<input onChange={()=>{hridoy("gender","Mail")}}  checked={number.gender==="Mail"} type="radio" name="gender"/>Mail
-<input onChange={()=>{hridoy("gender",'femail')}}  checked={number.gender==="femail"} type="radio" name="gender"/>femail
-<br />
-
-<button>submit</button>
+<Route   path="/" element={<Demo/>}/>
+<Route path="/loging"  element={ <Loging/>}/>
+<Route path="*" element={ <Headar/> }/>
 
 
 
 
+  </Routes>
 
 
 
-
-</form>
+</BrowserRouter>
 
 
 
